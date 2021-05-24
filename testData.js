f15561a29d5bf3674f863e7b1396784e9bb61c2b
xutongbao <1183391880@qq.com>
2021-05-24 09:28:23 +0800

    1

---
+++ testData.js
@@ -0,0 +1,158 @@
+
+const voiceData ={
+  "state": 1,
+  "message": "查询成功",
+  "success": true,
+  "data": {
+      "list": [
+          {
+              "taskId": 70,
+              "taskName": "1",
+              "taskDescription": "2",
+              "taskStatus": 1,
+              "executeTime": null,
+              "taskStartTime": "1621958400000",
+              "taskEndTime": "1621958400000",
+              "creator": "jiangziya",
+              "createTime": "1620785506078",
+              "rules": null,
+              "createUserId": 3953,
+              "companyId": 334
+          },
+          {
+              "taskId": 69,
+              "taskName": "1",
+              "taskDescription": "2",
+              "taskStatus": 0,
+              "executeTime": null,
+              "taskStartTime": "1620748800000",
+              "taskEndTime": "1620748800000",
+              "creator": "jiangziya",
+              "createTime": "1620785373337",
+              "rules": null,
+              "createUserId": 3953,
+              "companyId": 334
+          },
+          {
+              "taskId": 61,
+              "taskName": "1",
+              "taskDescription": "2",
+              "taskStatus": 0,
+              "executeTime": null,
+              "taskStartTime": "1621353600000",
+              "taskEndTime": "1621440000000",
+              "creator": "jiangziya",
+              "createTime": "1620730595716",
+              "rules": null,
+              "createUserId": 3953,
+              "companyId": 334
+          },
+          {
+              "taskId": 60,
+              "taskName": "1",
+              "taskDescription": "2",
+              "taskStatus": 0,
+              "executeTime": null,
+              "taskStartTime": "1621267200000",
+              "taskEndTime": "1624118400000",
+              "creator": "jiangziya",
+              "createTime": "1620730525120",
+              "rules": null,
+              "createUserId": 3953,
+              "companyId": 334
+          },
+          {
+              "taskId": 59,
+              "taskName": "1",
+              "taskDescription": "2",
+              "taskStatus": 0,
+              "executeTime": null,
+              "taskStartTime": "1620662400000",
+              "taskEndTime": "1620835200000",
+              "creator": "jiangziya",
+              "createTime": "1620730324868",
+              "rules": null,
+              "createUserId": 3953,
+              "companyId": 334
+          },
+          {
+              "taskId": 58,
+              "taskName": "1",
+              "taskDescription": "2",
+              "taskStatus": 0,
+              "executeTime": null,
+              "taskStartTime": "1620662400000",
+              "taskEndTime": "1620835200000",
+              "creator": "jiangziya",
+              "createTime": "1620730304192",
+              "rules": null,
+              "createUserId": 3953,
+              "companyId": 334
+          },
+          {
+              "taskId": 57,
+              "taskName": "1",
+              "taskDescription": "2",
+              "taskStatus": 0,
+              "executeTime": null,
+              "taskStartTime": "1620662400000",
+              "taskEndTime": "1620835200000",
+              "creator": "jiangziya",
+              "createTime": "1620730295517",
+              "rules": null,
+              "createUserId": 3953,
+              "companyId": 334
+          },
+          {
+              "taskId": 56,
+              "taskName": "1",
+              "taskDescription": "2",
+              "taskStatus": 0,
+              "executeTime": null,
+              "taskStartTime": "1621353600000",
+              "taskEndTime": "1621785600000",
+              "creator": "jiangziya",
+              "createTime": "1620730267745",
+              "rules": null,
+              "createUserId": 3953,
+              "companyId": 334
+          },
+          {
+              "taskId": 55,
+              "taskName": "1",
+              "taskDescription": "2",
+              "taskStatus": 0,
+              "executeTime": null,
+              "taskStartTime": "1621353600000",
+              "taskEndTime": "1621785600000",
+              "creator": "jiangziya",
+              "createTime": "1620730142110",
+              "rules": null,
+              "createUserId": 3953,
+              "companyId": 334
+          },
+          {
+              "taskId": 54,
+              "taskName": "1",
+              "taskDescription": "2",
+              "taskStatus": 0,
+              "executeTime": null,
+              "taskStartTime": "1621353600000",
+              "taskEndTime": "1621785600000",
+              "creator": "jiangziya",
+              "createTime": "1620730120583",
+              "rules": null,
+              "createUserId": 3953,
+              "companyId": 334
+          }
+      ],
+      "pageNum": 1,
+      "pageSize": 10,
+      "totalPage": 4,
+      "totalCount": 39
+  }
+}
+
+module.exports = {
+  voiceData
+}
