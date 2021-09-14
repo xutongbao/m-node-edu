d3a96b860b0e35f2ca8effdf7e3bc2c94d7917bb
xutongbao <1183391880@qq.com>
2021-09-14 14:20:11 +0800

    1

--- csdn/csdn.js
+++ csdn/csdn.js
@@ -1,4 +1,6 @@
 const fs = require('fs')
+const html2md = require('html-to-md')
+const { NodeHtmlMarkdown } = require('node-html-markdown')
 
 //删除文件夹下的所有文件和子文件夹，不删除该文件夹
 const removeFileDir = (path) => {
@@ -15,40 +17,45 @@ const removeFileDir = (path) => {
 }
 //搜索
 const dataSearch = (req, res) => {
-  let htmlStr = fs.readFileSync(__dirname + '/data.xml', 'utf8')
+  //let htmlStr = fs.readFileSync(__dirname + '/data.xml', 'utf8')
+  let htmlStr = fs.readFileSync(__dirname + '/dataMiddle.xml', 'utf8')
+  //let htmlStr = fs.readFileSync(__dirname + '/dataAll.xml', 'utf8')
+
   htmlStr = htmlStr
-    .replace(/&lt;/g, '<')
-    .replace(/&gt;/g, '>')
-    .replace(/&quot;/g, '"')
-    .replace(/\n/g, '')
+    // .replace(/&lt;/g, '<')
+    // .replace(/&gt;/g, '>')
+    // .replace(/&quot;/g, '"')
+    //.replace(/\n/g, '')
   let itemArr = []
   htmlStr.replace(/<item(([\s\S])*?)<\/item>/g, (word) => {
     itemArr.push(word)
     return word
   })
-  removeFileDir(`${__dirname}/md`)
-  itemArr.forEach((item) => {
+
+  const outputDir = `D:/source/m-apps/demo/vuepress/m-app2/test/blogs/md`
+  console.log(outputDir)
+  //removeFileDir(`${__dirname}/md`)
+  removeFileDir(outputDir)
+  itemArr.forEach((item, index) => {
     let title = ''
     item.replace(/<title(([\s\S])*?)<\/title>/g, (word) => {
       title = word.slice(7, word.length - 8)
-      console.log(title)
       return word
     })
     let content = ''
     item.replace(/<!\[CDATA\[(([\s\S])*?)]]>/g, (word) => {
       content = word.slice(9, word.length - 3)
-      console.log(content)
       return word
     })
+    //content = `# ${title}\n${html2md(content)}`
+    content = `# ${title}\n${NodeHtmlMarkdown.translate(content)}`
     fs.writeFile(
-      __dirname + `/md/${title}.md`,
-      `# ${title}
-      ${content}`,
+      `${outputDir}/${index}.md`,
+      content,
       function (err) {
         if (err) {
-          return console.log(err)
+          return console.log('错误', err)
         }
-        console.log('The file was saved!')
       }
     )
   })
