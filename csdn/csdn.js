da6f725ca6c392b6d631ba9d94b272b0ac7e8115
xutongbao <1183391880@qq.com>
2021-09-15 09:48:00 +0800

    1

--- csdn/csdn.js
+++ csdn/csdn.js
@@ -1,6 +1,63 @@
 const fs = require('fs')
 const html2md = require('html-to-md')
 const { NodeHtmlMarkdown } = require('node-html-markdown')
+//搜索
+const dataSearch = (req, res) => {
+  const { dataType = 0 } = req.body
+
+  let dataUrl = {
+    0: '/data.xml',
+    1: '/dataMiddle.xml',
+    2: '/data_9142.xml',
+  }[dataType]
+  const htmlStr = fs.readFileSync(__dirname + dataUrl, 'utf8')
+
+  //提取所有item标签存入数组中
+  let itemArr = []
+  htmlStr.replace(/<item(([\s\S])*?)<\/item>/g, (word) => {
+    itemArr.push(word)
+    return word
+  })
+
+  //md文件输出路径
+  const outputDir = `D:/source/blog/src/md`
+  removeFileDir(outputDir)
+
+  itemArr.forEach((item, index) => {
+    //提取标题
+    let title = ''
+    item.replace(/<title(([\s\S])*?)<\/title>/g, (word) => {
+      title = word.slice(7, word.length - 8)
+      return word
+    })
+    //提取博客内容
+    let content = ''
+    item.replace(/<!\[CDATA\[(([\s\S])*?)]]>/g, (word) => {
+      content = word.slice(9, word.length - 3)
+      return word
+    })
+    //把标题和内容组织在一起，形成md文件
+    const mdFile = `---
+title: '${title}'
+---   
+${html2md(content)}`
+
+    //创建md文件
+    fs.writeFile(`${outputDir}/${index}.md`, mdFile, function (err) {
+      if (err) {
+        return console.log('错误', err)
+      }
+    })
+  })
+
+  res.send({
+    state: 1,
+    data: {
+      count: itemArr.length,
+    },
+    message: 'md文件创建成功',
+  })
+}
 
 //删除文件夹下的所有文件和子文件夹，不删除该文件夹
 const removeFileDir = (path) => {
@@ -14,55 +71,6 @@ const removeFileDir = (path) => {
     }
   }
   //fs.rmdirSync(path)
-}
-//搜索
-const dataSearch = (req, res) => {
-  let htmlStr = fs.readFileSync(__dirname + '/data.xml', 'utf8')
-  //let htmlStr = fs.readFileSync(__dirname + '/dataMiddle.xml', 'utf8')
-  //let htmlStr = fs.readFileSync(__dirname + '/dataAll.xml', 'utf8')
-
-  let itemArr = []
-  htmlStr.replace(/<item(([\s\S])*?)<\/item>/g, (word) => {
-    itemArr.push(word)
-    return word
-  })
-
-  const outputDir = `D:/source/blog/src/md`
-  removeFileDir(outputDir)
-  itemArr.forEach((item, index) => {
-    let title = ''
-    item.replace(/<title(([\s\S])*?)<\/title>/g, (word) => {
-      title = word.slice(7, word.length - 8)
-      return word
-    })
-    let content = ''
-    item.replace(/<!\[CDATA\[(([\s\S])*?)]]>/g, (word) => {
-      content = word.slice(9, word.length - 3)
-      return word
-    })
-    content = 
-`---
-title: '${title}'
----   
-${html2md(content)}`
-    fs.writeFile(
-      `${outputDir}/${index}.md`,
-      content,
-      function (err) {
-        if (err) {
-          return console.log('错误', err)
-        }
-      }
-    )
-  })
-  res.send({
-    state: 1,
-    data: {
-      htmlStr,
-      itemArr,
-    },
-    message: '搜索成功',
-  })
 }
 
 module.exports = {
