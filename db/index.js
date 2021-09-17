cba06f671fd386236c0511f21692856d15213d96
xutongbao <1183391880@qq.com>
2021-09-17 10:55:32 +0800

    数据库

--- db/index.js
+++ db/index.js
@@ -1,6 +1,11 @@
-var sqlite3 = require('sqlite3').verbose()
-var db = new sqlite3.Database('./my1.db')
-
+const sqlite3 = require('sqlite3').verbose()
+console.log('db', process.env.PORT)
+let db = {}
+if (process.env.PORT === 81) {
+  db = new sqlite3.Database('./my_81.db')
+} else {
+  db = new sqlite3.Database('./my_online.db')
+}
 //执行sql语句
 const runSql = async (sql) => {
   return new Promise(async (resolve, reject) => {
