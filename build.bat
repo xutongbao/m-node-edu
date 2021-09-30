e742616415270678c9267fafd0fc8600d5d16159
xutongbao <1183391880@qq.com>
2021-09-30 14:56:54 +0800

    优化

--- build.bat
+++ build.bat
@@ -5,9 +5,8 @@ echo install done
 @REM 设置git仓库名称
 set gitRepositorieName=m-node-edu
 
-
 @REM 复制编译好的的代码到新目录
-xcopy \Jenkins\workspace\%gitRepositorieName% \temp\%gitRepositorieName%\ /Y /E
+@REM xcopy \Jenkins\workspace\%gitRepositorieName% \temp\%gitRepositorieName%\ /Y /E
 
 @REM 设置环境变量并执行js脚本发送邮件通知、添加构建记录
 set IP=39.97.238.175
