6bd96306c7c246d8b395d838a665f35e2bb592bb
xutongbao <1183391880@qq.com>
2021-10-09 11:12:41 +0800

    优化

---
+++ dbFile/my_dev.sql
@@ -0,0 +1,130 @@
+CREATE TABLE myLogs (
+    id         TEXT,
+    addtime    TEXT,
+    edittime   TEXT,
+    path       TEXT,
+    username   TEXT,
+    browser    TEXT,
+    errorTitle TEXT,
+    detail     TEXT,
+    status     TEXT
+);
+
+CREATE TABLE projectTest (
+    uid                VARCHAR (100) NOT NULL,
+    name               VARCHAR (100) NOT NULL,
+    gitRepositorieName VARCHAR (100) NOT NULL,
+    branch             VARCHAR (100) NOT NULL,
+    url                VARCHAR (300) NOT NULL,
+    remarks            VARCHAR (100) NOT NULL,
+    addtime            VARCHAR (100) NOT NULL,
+    edittime           VARCHAR (100) NOT NULL,
+    PRIMARY KEY (
+        uid
+    )
+);
+
+INSERT INTO projectTest (
+                            uid,
+                            name,
+                            gitRepositorieName,
+                            branch,
+                            url,
+                            remarks,
+                            addtime,
+                            edittime
+                        )
+                        VALUES (
+                            '1632983530971',
+                            '知了好客',
+                            'edu',
+                            'origin/feature/newName',
+                            'http://39.97.238.175:81/edu/origin/feature/newName',
+                            '知了好客改为知了好学',
+                            '1632983530971',
+                            ''
+                        );
+
+INSERT INTO projectTest (
+                            uid,
+                            name,
+                            gitRepositorieName,
+                            branch,
+                            url,
+                            remarks,
+                            addtime,
+                            edittime
+                        )
+                        VALUES (
+                            '1632983182375',
+                            '知了好客',
+                            'edu',
+                            'origin/release/ui20210720',
+                            'http://39.97.238.175:81/edu/origin/release/ui20210720',
+                            '和线上代码同步',
+                            '1632983182376',
+                            ''
+                        );
+
+INSERT INTO projectTest (
+                            uid,
+                            name,
+                            gitRepositorieName,
+                            branch,
+                            url,
+                            remarks,
+                            addtime,
+                            edittime
+                        )
+                        VALUES (
+                            '1632982857927',
+                            '知了好客',
+                            'edu',
+                            'origin/feature/login',
+                            'http://39.97.238.175:81/edu/origin/feature/login',
+                            '开发中',
+                            '1632982857927',
+                            ''
+                        );
+
+INSERT INTO projectTest (
+                            uid,
+                            name,
+                            gitRepositorieName,
+                            branch,
+                            url,
+                            remarks,
+                            addtime,
+                            edittime
+                        )
+                        VALUES (
+                            '1632982651006',
+                            '探马',
+                            'tan',
+                            'origin/feature/login',
+                            'http://39.97.238.175:81/tan/origin/feature/login',
+                            '自动',
+                            '1632982651006',
+                            ''
+                        );
+
+INSERT INTO projectTest (
+                            uid,
+                            name,
+                            gitRepositorieName,
+                            branch,
+                            url,
+                            remarks,
+                            addtime,
+                            edittime
+                        )
+                        VALUES (
+                            '1632982424994',
+                            '探马',
+                            'tan',
+                            'origin/master',
+                            'http://39.97.238.175:81/tan/origin/master',
+                            '自动',
+                            '1632982424994',
+                            ''
+                        );
