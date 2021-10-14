bdcdcc2218c297bf8c9a64e19de22e38f0de10e0
xutongbao <1183391880@qq.com>
2021-10-14 16:40:02 +0800

    优化

--- run.bat
+++ run.bat
@@ -4,4 +4,4 @@ call yarn -v
 call yarn
 @REM name会自动应用到pm2配置文件ecosystem.config.js的name字段
 set name=%1
-@REM call pm2 start ecosystem.config.js --env production
+call pm2 start ecosystem.config.js --env production
