12d009875d1a8dc35634d3c9cf733d70ec546f62
xutongbao <1183391880@qq.com>
2021-10-14 16:18:05 +0800

    优化

--- run.js
+++ run.js
@@ -1,4 +1,4 @@
-const spawn = require('cross-spawn');
+const spawn = require('cross-spawn')
 
 spawn.sync('yarn -v', [], { stdio: 'inherit' })
 spawn.sync('run.bat origin/master', [], { stdio: 'inherit' })
