0de976dffe0c333c24db3c77d70c543977f1228a
xutongbao <1183391880@qq.com>
2021-10-14 17:31:04 +0800

    优化

--- ecosystem.config.js
+++ ecosystem.config.js
@@ -1,10 +1,12 @@
 module.exports = {
   apps: [
     {
+      name: 'source',
       script: './app.js',
       watch: true,
       ignore_watch: ['node_modules', 'log', 'dbFile', '.git'],
       node_args: ['--inspect'],
+      force: true,
       env_development: {
         PORT: 81,
         NODE_ENV: 'development'
