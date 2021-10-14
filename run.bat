b5f1bc403a3d18193a2f0f42e83ba954d0364323
xutongbao <1183391880@qq.com>
2021-10-14 18:00:34 +0800

    优化

--- run.bat
+++ run.bat
@@ -3,5 +3,6 @@ cd /temp/m-node-edu/%1
 call yarn -v
 call yarn
 @REM name会自动应用到pm2配置文件ecosystem.config.js的name字段
-set name=%1
+set tempName=%1
+set name=%tempName:\=_%
 call pm2 start ecosystem.config.js --env production
