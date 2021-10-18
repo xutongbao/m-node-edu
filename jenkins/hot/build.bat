ac053935d39f7d383d4d79597f9f325457914f56
xutongbao <1183391880@qq.com>
2021-10-18 19:21:58 +0800

    优化

--- jenkins/hot/build.bat
+++ jenkins/hot/build.bat
@@ -7,7 +7,7 @@ set gitRepositorieName=m-node-edu
 set pipeline=-hot
 
 @REM 复制编译好的的代码到新目录
-xcopy \Jenkins\workspace\%gitRepositorieName%%hot% \temp\%gitRepositorieName%\%branch:/=\%\ /Y /E /exclude:exclude.txt
+xcopy \Jenkins\workspace\%gitRepositorieName%%pipeline% \temp\%gitRepositorieName%\%branch:/=\%\ /Y /E /exclude:exclude.txt
 
 @REM 设置环境变量并执行js脚本发送邮件通知、添加构建记录
 set computername=%computername%
