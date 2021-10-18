786d56c2b15b7bca69d4116a927af1232ef3d7c1
xutongbao <1183391880@qq.com>
2021-10-18 16:54:48 +0800

    优化

---
+++ jenkins/hot/run.bat
@@ -0,0 +1,14 @@
+@REM 接口/api/jenkins/run执行这个批处理
+echo %0 %1
+set historyPath=%cd%
+echo %historyPath%
+cd /temp/m-node-edu/%1
+call yarn -v
+call yarn
+@REM name会自动应用到pm2配置文件ecosystem.config.js的name字段
+set branch=%1
+set name=%branch:\=_%
+set watch=false
+call pm2 start ecosystem.config.js --env production
+@REM 跳转到原路径
+cd %historyPath%
