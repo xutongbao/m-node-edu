1ebcb54e7295ca11d35c2c27f7dcff7d7a093b3a
xutongbao <1183391880@qq.com>
2021-10-20 17:31:53 +0800

    优化

--- jenkins/test/build.bat
+++ jenkins/test/build.bat
@@ -4,10 +4,10 @@ echo install done
 
 @REM 设置git仓库名称
 set gitRepositorieName=m-node-edu
-set pipeline=-test
+set pipeline=test-
 
 @REM 复制编译好的的代码到新目录
-xcopy \Jenkins\workspace\%gitRepositorieName%%pipeline% \temp\%gitRepositorieName%\%branch:/=\%\ /Y /E /exclude:exclude.txt
+xcopy \Jenkins\workspace\%pipeline%%gitRepositorieName% \temp\%gitRepositorieName%\%branch:/=\%\ /Y /E /exclude:exclude.txt
 
 @REM 设置环境变量并执行js脚本发送邮件通知、添加构建记录
 set computername=%computername%
