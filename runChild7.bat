97d47c57de07a40d0a74bb8164597867968ed842
xutongbao <1183391880@qq.com>
2021-10-25 16:01:18 +0800

    优化

--- runChild7.bat
+++ runChild7.bat
@@ -1,7 +1,14 @@
 @REM 开机启动pm2服务，80端口和81端口
+
 CD /Jenkins/workspace/prod-m-node-edu-ice
-call yarn dev2
+
+set name=source_scripts_m_node_edu_dev2 
+call pm2 start ecosystem.config.js --env development
+
 CD /Jenkins/workspace/prod-m-node-edu
-call yarn serve2
+
+set name=source_scripts_m_node_edu_serve2 
+call pm2 start ecosystem.config.js --env production 
+
 echo success
 pause
