1c23966bac5b10624e29f06a2decf08fe3405988
xutongbao <1183391880@qq.com>
2021-10-26 16:25:22 +0800

    优化

--- utils/tools.js
+++ utils/tools.js
@@ -140,7 +140,7 @@ let transporter = {}
 const emailInit = async () => {
   // Generate test SMTP service account from ethereal.email
   // Only needed if you don't have a real mail account for testing
-  let testAccount = await nodemailer.createTestAccount()
+  //let testAccount = await nodemailer.createTestAccount()
 
   // create reusable transporter object using the default SMTP transport
   transporter = nodemailer.createTransport({
