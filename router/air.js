13ce878011a94aa58b0b6a03be1c5fcee56d5550
xutongbao <1183391880@qq.com>
2021-11-08 10:17:43 +0800

    优化

--- router/air.js
+++ router/air.js
@@ -23,20 +23,20 @@ const {
 } = require('../air/app/fields/fields')
 
 const air = (app) => {
-  //应用接口
-  app.post('/api/app/router/searchAll', routerSearchAll)
-  app.post('/api/app/router/search', routerSearch)
-  app.post('/api/app/router/add', routerAdd)
-  app.post('/api/app/router/delete', routerDelete)
-  app.post('/api/app/router/edit', routerEdit)
+  //应用列表
+  app.post('/api/app/list/searchAll', routerSearchAll)
+  app.post('/api/app/list/search', routerSearch)
+  app.post('/api/app/list/add', routerAdd)
+  app.post('/api/app/list/delete', routerDelete)
+  app.post('/api/app/list/edit', routerEdit)
 
-  //应用对应的表格接口
+  //应用内容
   app.post('/api/app/table/search', tableSearch)
   app.post('/api/app/table/add', tableAdd)
   app.post('/api/app/table/delete', tableDelete)
   app.post('/api/app/table/edit', tableEdit)
 
-  //应用字段对应的表格接口
+  //应用字段
   app.post('/api/app/fields/searchAll', fieldsSearchAll)
   app.post('/api/app/fields/search', fieldsSearch)
   app.post('/api/app/fields/add', fieldsAdd)
