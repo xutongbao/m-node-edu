9aca70c445d6e6946d983d146c4e69f4d0c4f4da
xutongbao <1183391880@qq.com>
2021-11-13 15:19:35 +0800

    优化

--- air/process/data.js
+++ air/process/data.js
@@ -9,26 +9,44 @@ let dataArr = [
     tree: [
       {
         name: '申请人',
+        content: '商之讯',
         belongCategory: '0',
         status: true,
         id: 1622771045562,
+        color: 'blue',
+        positon: {
+          rolIndex: 1,
+          colIndex: 2,
+        },
         children: [
           {
             name: '审批人',
+            content: '申请人自选 不限范围 多选 依次审批',
             belongCategory: 1622771045562,
             status: false,
             id: 1622771052842,
+            color: 'orange',
+            positon: {
+              rolIndex: 3,
+              colIndex: 2,
+            },
             children: [
               {
                 name: '抄送人',
+                content: '申请人自选',
                 belongCategory: 1622771052842,
                 status: false,
-                id: 1636424256035
-              }
-            ]
-          }
-        ]
-      }
+                id: 1636424256035,
+                color: 'green',
+                positon: {
+                  rolIndex: 5,
+                  colIndex: 2,
+                },
+              },
+            ],
+          },
+        ],
+      },
     ]
   },
   {
@@ -38,27 +56,71 @@ let dataArr = [
     title: '报销',
     tree: [
       {
-        name: '流程开始',
+        name: '申请人',
+        content: '商之讯',
         belongCategory: '0',
         status: true,
         id: 1622771045562,
+        color: 'blue',
+        positon: {
+          rolIndex: 1,
+          colIndex: 2,
+        },
         children: [
           {
             name: '审批人',
+            content: '申请人自选 不限范围 多选 依次审批',
             belongCategory: 1622771045562,
             status: false,
             id: 1622771052842,
+            color: 'orange',
+            positon: {
+              rolIndex: 3,
+              colIndex: 1,
+            },
             children: [
               {
                 name: '抄送人',
+                content: '申请人自选',
                 belongCategory: 1622771052842,
                 status: false,
-                id: 1636424256035
-              }
-            ]
-          }
-        ]
-      }
+                id: 1636424256035,
+                color: 'green',
+                positon: {
+                  rolIndex: 5,
+                  colIndex: 1,
+                },
+              },
+            ],
+          },
+          {
+            name: '审批人',
+            content: '申请人自选 不限范围 多选 依次审批',
+            belongCategory: 1622771045562,
+            status: false,
+            id: 1622771052842,
+            color: 'orange',
+            positon: {
+              rolIndex: 3,
+              colIndex: 3,
+            },
+            children: [
+              {
+                name: '抄送人',
+                content: '申请人自选',
+                belongCategory: 1622771052842,
+                status: false,
+                id: 1636424256035,
+                color: 'green',
+                positon: {
+                  rolIndex: 5,
+                  colIndex: 3,
+                },
+              },
+            ],
+          },
+        ],
+      },
     ]
   },
   {
@@ -68,27 +130,223 @@ let dataArr = [
     title: '出差',
     tree: [
       {
-        name: '流程起点',
+        name: '申请人',
+        content: '商之讯',
         belongCategory: '0',
         status: true,
         id: 1622771045562,
+        color: 'blue',
+        positon: {
+          rolIndex: 1,
+          colIndex: 4,
+        },
         children: [
           {
             name: '审批人',
+            content: '申请人自选 不限范围 多选 依次审批',
             belongCategory: 1622771045562,
             status: false,
             id: 1622771052842,
+            color: 'orange',
+            positon: {
+              rolIndex: 3,
+              colIndex: 2,
+            },
             children: [
               {
                 name: '抄送人',
+                content: '申请人自选',
                 belongCategory: 1622771052842,
                 status: false,
-                id: 1636424256035
-              }
-            ]
-          }
-        ]
-      }
+                id: 1636424256035,
+                color: 'green',
+                positon: {
+                  rolIndex: 5,
+                  colIndex: 2,
+                },
+              },
+            ],
+          },
+          {
+            name: '审批人',
+            content: '申请人自选 不限范围 多选 依次审批',
+            belongCategory: 1622771045562,
+            status: false,
+            id: 1622771052842,
+            color: 'orange',
+            positon: {
+              rolIndex: 3,
+              colIndex: 4,
+            },
+            children: [
+              {
+                name: '抄送人',
+                content: '申请人自选',
+                belongCategory: 1622771052842,
+                status: false,
+                id: 1636424256035,
+                color: 'green',
+                positon: {
+                  rolIndex: 5,
+                  colIndex: 4,
+                },
+              },
+            ],
+          },
+          {
+            name: '审批人',
+            content: '申请人自选 不限范围 多选 依次审批',
+            belongCategory: 1622771045562,
+            status: false,
+            id: 1622771052842,
+            color: 'orange',
+            positon: {
+              rolIndex: 3,
+              colIndex: 6,
+            },
+            children: [
+              {
+                name: '抄送人',
+                content: '申请人自选',
+                belongCategory: 1622771052842,
+                status: false,
+                id: 1636424256035,
+                color: 'green',
+                positon: {
+                  rolIndex: 5,
+                  colIndex: 6,
+                },
+              },
+            ],
+          },
+        ],
+      },
+    ]
+  },
+  {
+    id: '3',
+    key: '3',
+    path: '/light/index/content?id=3',
+    title: '活动',
+    tree: [
+      {
+        name: '申请人',
+        content: '商之讯',
+        belongCategory: '0',
+        status: true,
+        id: 1622771045562,
+        color: 'blue',
+        positon: {
+          rolIndex: 1,
+          colIndex: 6,
+        },
+        children: [
+          {
+            name: '审批人',
+            content: '申请人自选 不限范围 多选 依次审批',
+            belongCategory: 1622771045562,
+            status: false,
+            id: 1622771052842,
+            color: 'orange',
+            positon: {
+              rolIndex: 3,
+              colIndex: 3,
+            },
+            children: [
+              {
+                name: '抄送人',
+                content: '申请人自选',
+                belongCategory: 1622771052842,
+                status: false,
+                id: 1636424256035,
+                color: 'green',
+                positon: {
+                  rolIndex: 5,
+                  colIndex: 3,
+                },
+              },
+            ],
+          },
+          {
+            name: '审批人',
+            content: '申请人自选 不限范围 多选 依次审批',
+            belongCategory: 1622771045562,
+            status: false,
+            id: 1622771052842,
+            color: 'orange',
+            positon: {
+              rolIndex: 3,
+              colIndex: 5,
+            },
+            children: [
+              {
+                name: '抄送人',
+                content: '申请人自选',
+                belongCategory: 1622771052842,
+                status: false,
+                id: 1636424256035,
+                color: 'green',
+                positon: {
+                  rolIndex: 5,
+                  colIndex: 5,
+                },
+              },
+            ],
+          },
+          {
+            name: '审批人',
+            content: '申请人自选 不限范围 多选 依次审批',
+            belongCategory: 1622771045562,
+            status: false,
+            id: 1622771052842,
+            color: 'orange',
+            positon: {
+              rolIndex: 3,
+              colIndex: 7,
+            },
+            children: [
+              {
+                name: '抄送人',
+                content: '申请人自选',
+                belongCategory: 1622771052842,
+                status: false,
+                id: 1636424256035,
+                color: 'green',
+                positon: {
+                  rolIndex: 5,
+                  colIndex: 7,
+                },
+              },
+            ],
+          },
+          {
+            name: '审批人',
+            content: '申请人自选 不限范围 多选 依次审批',
+            belongCategory: 1622771045562,
+            status: false,
+            id: 1622771052842,
+            color: 'orange',
+            positon: {
+              rolIndex: 3,
+              colIndex: 9,
+            },
+            children: [
+              {
+                name: '抄送人',
+                content: '申请人自选',
+                belongCategory: 1622771052842,
+                status: false,
+                id: 1636424256035,
+                color: 'green',
+                positon: {
+                  rolIndex: 5,
+                  colIndex: 9,
+                },
+              },
+            ],
+          },
+        ],
+      },
     ]
   }
 ]
---
+++ light/jenkins/linuxUploat.bat
@@ -0,0 +1,1 @@
+plink -C -ssh -pw "5XY&m$5j47UE7O!W" root@39.107.44.208
