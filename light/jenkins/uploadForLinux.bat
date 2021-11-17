8bbb7e2636099c5df475b72fd3906465a9e04099
xutongbao <1183391880@qq.com>
2021-11-17 11:35:15 +0800

    优化

--- light/jenkins/uploadForLinux.bat
+++ light/jenkins/uploadForLinux.bat
@@ -1,5 +1,7 @@
 echo %0 %1 %2
 7z.exe a \temp\zip\buildForLinux.tar %1
-call C:\ftp\sftp.bat
-call C:\ftp\ssh.bat
+cmd.exe /c /ftp/sftp.bat
+cmd.exe /c /ftp/ssh.bat
+@REM call C:\ftp\sftp.bat
+@REM call C:\ftp\ssh.bat
 echo unzip success
